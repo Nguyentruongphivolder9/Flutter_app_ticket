c2142847497ee20921b5c0a3b5467a1d1e4f075a
Akatsuki666 <nguyentruongphi15032003@gmail.com>
2022-10-08 23:38:16 +0700

    ticket_view

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,100 @@
+import 'package:app_demo_ticket/screens/ticket_view.dart';
+import 'package:app_demo_ticket/utils/app_styles.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Good morning", style: Styles.headLineStyle3,
+                        ),
+                        const Gap(5),
+                        Text(
+                          "Book Tickets", style: Styles.headLineStyle2,
+                        )
+                      ],
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            "assets/images/img_logo.png"
+                          )
+                        )
+                      ),
+                    )
+                  ],
+                ),
+                const Gap(25),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xFFF4F6FD)
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                  child: Row(
+                    children: [
+                      const Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xFFBFC250)),
+                      Text(
+                        "Search",
+                        style: Styles.headLineStyle4,
+                     )
+                   ],
+                  ),
+                ),
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Upcoming Flights", style: Styles.headLineStyle2,),
+                    InkWell(
+                      onTap: () {
+                        print("You are tapped");
+                      },
+                      child: Text("View all", style: Styles.textStyle.copyWith(color: Styles.primaryColor)),
+                    )
+                  ],
+                )
+              ]
+            ),
+          ),
+          const Gap(15),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20),
+            child: Row(
+              children: [
+                TicketView(),
+                TicketView()
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
